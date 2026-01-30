bba1fd0eaf74d49500ce4a0f19dbfe121d38a1a4
KTH <sauce0428@gmail.com>
2026-01-30 14:20:03 +0900

    [추가] MemberMapper.java, MemberService.java, MemberMapper.xml, MemberServiceimpl 생성

---
+++ SpringMyBatisMember/src/main/java/com/zeus/controller/MemberController.java
@@ -0,0 +1,17 @@
+package com.zeus.controller;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Controller
+@MapperScan(basePackages = "com.zeus.mapper")
+@RequestMapping("/member")
+public class MemberController {
+	@Autowired
+	private
+}
